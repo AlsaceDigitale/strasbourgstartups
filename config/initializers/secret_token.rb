66d0bd266cf021ab6c6976bff2144926f6d3767e
Julien Gamba <jgamba@aius.u-strasbg.fr>
2013-06-28 17:05:12 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Strasbourgstartups::Application.config.secret_key_base = '7c369561fdd14d788921057b9b634e029b10edd8718c0f967895275c50666ec638e54bf73b7bc4c990706d43daaac2cc2a0a1118b224b8849c2333ab3decf55b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Strasbourgstartups::Application.config.session_store :cookie_store, key: '_strasbourgstartups_session'
