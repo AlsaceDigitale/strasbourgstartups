77a6b6642065b1fcb71edf6609ecf0705f5d1683
Julien Gamba <jgamba@aius.u-strasbg.fr>
2013-07-01 09:33:29 +0200

    Basic map display.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,9 +1,16 @@
 class Startup
   include Mongoid::Document
+  include Mongoid::Paperclip
+
+  include Mongoid::Document
   field :name, type: String
   field :url, type: String
   field :adress, type: String
   field :email, type: String
   field :description, type: String
   field :is_published, type: Boolean, deafult: false
+
+  field :coordinates, type: Array
+
+  has_mongoid_attached_file :logo
 end
