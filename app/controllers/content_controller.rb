7638d66b78bba0637577c9bc3e6d44f1bba33003
Yann Klis <yann.klis@novelys.com>
2013-07-18 17:26:02 +0200

    finished about page

---
+++ app/controllers/content_controller.rb
@@ -0,0 +1,5 @@
+class ContentController < ApplicationController
+  def about
+    @page_title = "About"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Strasbourgstartups::Application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-  
+
   resources :startups
   resources :headlines
+
+  get 'about' => 'content#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -44,7 +47,7 @@ Strasbourgstartups::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
