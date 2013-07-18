5e9c2a36f927d7d65404704cd6990f926b446015
Yann Klis <yann.klis@novelys.com>
2013-07-18 19:09:29 +0200

    wording

--- app/controllers/headlines_controller.rb
+++ app/controllers/headlines_controller.rb
@@ -1,6 +1,6 @@
 class HeadlinesController < ApplicationController
   def index
-    @page_title = "Latests news from Strasbourg Startups"
+    @page_title = "Latests news"
     @headlines = Headline.desc(:published_at)
   end
 end
