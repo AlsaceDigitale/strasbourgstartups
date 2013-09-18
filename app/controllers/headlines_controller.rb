ad4c0b7f8fb00af7a26c096bdde1aa4476939090
Yann Klis <yann.klis@novelys.com>
2013-09-18 16:59:28 +0200

    wording and direct link to article

--- app/controllers/headlines_controller.rb
+++ app/controllers/headlines_controller.rb
@@ -3,4 +3,8 @@ class HeadlinesController < ApplicationController
     @page_title = "Latest news / Dernières actualités"
     @headlines = Headline.desc(:published_at)
   end
+
+  def show
+    @headline = Headline.find params[:id]
+  end
 end
--- app/models/headline.rb
+++ app/models/headline.rb
@@ -1,12 +1,15 @@
 class Headline
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Slug
 
   field :title_fr, type: String
   field :title_en, type: String
   field :body_fr, type: String
   field :body_en, type: String
   field :published_at, type: Time
+
+  slug :title_en
 
   def title_en
     txt = read_attribute(:title_en)
