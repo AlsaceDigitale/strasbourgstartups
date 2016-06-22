29859ed6048f872762ddc2ef9a1b868b24c0efff
Yann Klis <yann.klis@gmail.com>
2016-06-22 02:20:48 +0200

    404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  unless Rails.configuration.action_controller.consider_all_requests_local
+    rescue_from ActionController::RoutingError,      with: :render_404
+    rescue_from ActionController::UnknownController, with: :render_404
+    rescue_from ActiveRecord::RecordNotFound,        with: :render_404
+  end
+
 private
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || admin_root_path
+  end
+
+  def render_404(exception)
+    render template: 'errors/404', status: 404
   end
 end
