6c3cde8968b320a088e48b18d10b4591d472a79d
Yann Klis <yann.klis@gmail.com>
2018-08-01 14:11:58 +0200

    disable asset_sync for the moment

--- config/application.rb
+++ config/application.rb
@@ -71,6 +71,7 @@ module Strasbourgstartups
       end
 
       AssetSync.configure do |config|
+        config.enable = false
         # Disable automatic run on precompile in order to attach to webpacker rake task
         config.run_on_precompile = false
         # The block should return an array of file paths
